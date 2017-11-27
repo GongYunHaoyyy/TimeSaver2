08e37aa80880be1f9daf60ba4005436ff951d659
龚云浩 <1501270490@qq.com>
2017-11-27 13:56:08 +0800

    first commit

---
+++ app/src/main/java/com/gongyunhaoyyy/timesaver/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.gongyunhaoyyy.timesaver;
+
+import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_login );
+        login=(Button)findViewById( R.id.login_bt );
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+        }
+        login.setOnClickListener( this );
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.login_bt:
+                Intent intent=new Intent( LoginActivity.this,MainActivity.class );
+                startActivity( intent );
+        }
+    }
+}
---
+++ app/src/main/java/com/gongyunhaoyyy/timesaver/MainActivity.java
@@ -0,0 +1,20 @@
+package com.gongyunhaoyyy.timesaver;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+        }
+    }
+}
